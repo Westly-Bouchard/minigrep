bd669b033efa6b71507b391c0844c859a8c1c238
Westly Bouchard <westly.bouchard@gmail.com>
2022-05-17 21:19:30 -0600

    parse arguments from command line

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    // Collect the arguments passed to the executable from the command line as an array of strings
+    let args: Vec<String> = env::args().collect();
+    
+    // Parse the query and the name of the file we want to search from the arguments
+    // Starting at 1 b/c the first element will be the name of the executable
+    let query = &args[1];
+    let filename = &args[2];
+
+    // Test to make sure it works
+    println!("Looking for {} in {}", query, filename);
 }
