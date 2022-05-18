3e011ba9347dc6ee3e615da85561b0b738c22f25
Westly Bouchard <westly.bouchard@gmail.com>
2022-05-18 09:17:27 -0600

    Moved app logic to `lib.rs`

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use std::{env, fs, process, error::Error};
+use std::{env, process};
+use minigrep::Config;
 
 fn main() {
     // Collect the arguments passed to the executable from the command line as an array of strings
@@ -13,38 +14,8 @@ fn main() {
     // Test to make sure it works
     println!("Looking for {} in {}", config.query, config.filename);
 
-    if let Err(e) = run(config) {
+    if let Err(e) = minigrep::run(config) {
         println!("Application error: {}", e);
         process::exit(1);
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    // Read contents to string
-    let contents = fs::read_to_string(config.filename)?;
-
-    // Test to make sure it works
-    println!("With text:\n{}", contents);
-
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        // We need three total args to successfully run the program
-        if args.len() < 3 {
-            return Err("Not enough arguments");
-        }
-
-        // Grab the query and filename from the arguments 
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config {query, filename})
     }
 }
