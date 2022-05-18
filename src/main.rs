8e2b4d696adbcd8a515d24947941263fee219aab
Westly Bouchard <westly.bouchard@gmail.com>
2022-05-17 21:30:14 -0600

    Read contents from file

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::env;
+use std::{env, fs};
 
 fn main() {
     // Collect the arguments passed to the executable from the command line as an array of strings
@@ -11,4 +11,11 @@ fn main() {
 
     // Test to make sure it works
     println!("Looking for {} in {}", query, filename);
+
+    // Read the contents of the file
+    let contents = fs::read_to_string(filename)
+        .expect("Failed to read the contents of the file");
+    
+    // Test to make sure it works
+    println!("With contents: {}", contents);
 }
