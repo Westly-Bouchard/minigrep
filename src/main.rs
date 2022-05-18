90543e4489bb343dbbe6af743b6d8e1d39155b95
Westly Bouchard <westly.bouchard@gmail.com>
2022-05-18 08:44:58 -0600

    Move command line parsing to function

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,8 @@ fn main() {
     // Collect the arguments passed to the executable from the command line as an array of strings
     let args: Vec<String> = env::args().collect();
     
-    // Parse the query and the name of the file we want to search from the arguments
-    // Starting at 1 b/c the first element will be the name of the executable
-    let query = &args[1];
-    let filename = &args[2];
+    // Parse command line arguments
+    let (query, filename) = parse_config(&args);
 
     // Test to make sure it works
     println!("Looking for {} in {}", query, filename);
@@ -18,4 +16,12 @@ fn main() {
     
     // Test to make sure it works
     println!("With contents: {}", contents);
+}
+
+fn parse_config(args: &[String]) -> (&str, &str) {
+    // Parse arguments, starts at 1 because the first argument is the name of the executable
+    let query = &args[1];
+    let filename = &args[2];
+
+    (query, filename)
 }
